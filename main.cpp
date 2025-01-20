e3131e39d249649b336c778c85e20af3cb404f18
Hossam Asad <hossamasad2004@gmail.com>
2025-01-20 15:31:53 -0500

    created a fullpath directory folder creation.

--- main.cpp
+++ main.cpp
@@ -5,15 +5,21 @@
 namespace fs = std::filesystem;
 
 int main(){
-
+    
+    std::string parentDir;
     std::string folderName;
 
-    std::cout<<"Enter the name of the folder to create:";
+    std::cout<<"Enter the parent directory path: ";
+    std::getline(std::cin, parentDir);
+
+    std::cout<<"Enter the name of the folder to create: ";
     std::getline(std::cin, folderName);
 
     try{
+
+        fs::path fullPath=fs::path(parentDir)/folderName;
         // create a directory
-        if (fs::create_directory(folderName)){
+        if (fs::create_directory(fullPath)){
             std::cout<<"Folder '" << folderName << "' created successfully!" << std::endl;
         }else{
             std::cout<<"Folder '" << folderName << "' already exists." << std::endl;
